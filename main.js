87b2712c7a5705fe691405ccca14dc54ccc60044
Tim Quattrochi <timq82@gmail.com>
2022-07-22 19:56:19 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+const nextItem = document.querySelector("#next");
+const previousItem = document.querySelector("#previous");
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+
+nextItem.addEventListener("click", () => {
+  if(counter >= carouselImages.length -1) return
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+  console.log(counter)
+});
+
+
+previousItem.addEventListener("click",() => {
+  if (counter <= 0) return
+
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = "translateX(" + (-size * counter )+ "px)";
+  console.log(counter)
+});
+
+carouselSlide.addEventListener("transitionend",() =>{
+  console.log(carouselImages[counter])
+  if (carouselImages[counter].id === "lastClone"){
+    carouselSlide.style.transition = "none"
+    counter = carouselImages.length - 2
+  carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+  console.log("fired")
+
+  }
+  if (carouselImages[counter].id === "firstClone"){
+    carouselSlide.style.transition = "none"
+    counter = carouselImages.length - counter
+  carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+  }
+})
+
+
